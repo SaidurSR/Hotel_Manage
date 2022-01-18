63c06279ef4537b2ed3dfae89eb18df2b63393ae
SaidurSR <forumct24@gmail.com>
2022-01-18 23:45:10 +0600

    Initial commit

---
+++ resources/views/about_us.blade.php
@@ -0,0 +1,59 @@
+@extends('frontlayout')
+@section('content')
+<div class="container my-4">
+	<div class="bg-light py-5">
+		<div class="container py-5">
+		  <div class="row mb-4">
+			<div class="col-lg-5">
+			  <h2 class="display-4 font-weight-light">About Us</h2>
+			  <p class="font-italic text-muted">Address: 12/8 Gopalpur, Tangail, Dhaka</p>
+			</div>
+		  </div>
+	  
+		  <div class="row text-center">
+			<!-- Team item-->
+			<div class="col-xl-3 col-sm-6 mb-5">
+			  <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-4.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+				<h5 class="mb-0">Cheapest Cost</h5><span class="small text-uppercase text-muted">at low cost</span>
+	
+			  </div>
+			</div>
+			<!-- End-->
+	  
+			<!-- Team item-->
+			<div class="col-xl-3 col-sm-6 mb-5">
+			  <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-3.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+				<h5 class="mb-0">Best Services</h5><span class="small text-uppercase text-muted">within time</span>
+				
+			  </div>
+			</div>
+			<!-- End-->
+	  
+			<!-- Team item-->
+			<div class="col-xl-3 col-sm-6 mb-5">
+			  <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-2.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+				<h5 class="mb-0">Best Comfort</h5><span class="small text-uppercase text-muted">with our Bigger rooms</span>
+				{{-- <ul class="social mb-0 list-inline mt-3">
+				  <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+				  <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+				  <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+				  <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+				</ul> --}}
+			  </div>
+			</div>
+			<!-- End-->
+	  
+			<!-- Team item-->
+			<div class="col-xl-3 col-sm-6 mb-5">
+			  <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-1.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+				<h5 class="mb-0">100% Satisfaction</h5><span class="small text-uppercase text-muted">with our services</span>
+				
+			  </div>
+			</div>
+			<!-- End-->
+	  
+		  </div>
+		</div>
+	  </div>
+</div>
+@endsection
